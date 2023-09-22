2dbe38f3e03e43daf42abea1385b1d50f2efd002
yandiaan <diansetiawan2121@gmail.com>
2023-09-22 11:09:10 +0700

    done

--- codelab3.kt
+++ codelab3.kt
@@ -1,17 +1,20 @@
 fun main() {
-    panggilNama()
-    tambah(2, 10)
-    println(namaLengkap("Dian", "Setiawan"))
+    panggilNama() // berfungsi untuk memanggil fungsi panggilNama()
+    tambah(2, 10) // berfungsi untuk memanggil fungsi tambah()
+    println(namaLengkap("Dian", "Setiawan")) // berfungsi untuk memanggil fungsi namaLengkap()
 }
 
+// fungsi panggilNama() adalah contoh fungsi tanpa parameter
 fun panggilNama() {
     println("Halo!! Nama Saya Dian")
 }
 
+// fungsi tambah() adalah contoh fungsi dengan parameter
 fun tambah(angka1: Int, angka2: Int) {
     println("jumlah angka: " + (angka1 + angka2))
 }
 
+// fungsi namaLengkap() adalah contoh dari fungsi dengan parameter dan return-value
 fun namaLengkap(firstName: String = "Rover", lastName: String): String {
     return "Halo, Nama Saya $firstName $lastName"
 }
---
+++ codelab4.kt
@@ -0,0 +1,16 @@
+// pada codelab4, saya membuat aplikasi mengenai program sederhana yang mencetak informasi mengenai buku
+
+fun main() {
+    // Perintah dibawah merupakan cara memanggil fungsi printBookInfo() yang telah dibuat di bawah
+    printBookInfo("The Catcher in the Rye", "J.D. Salinger", 1951)
+    printBookInfo("To Kill a Mockingbird", "Harper Lee", 1960)
+    printBookInfo("1984", "George Orwell", 1949)
+    printBookInfo("The Great Gatsby", "F. Scott Fitzgerald", 1925)
+}
+
+fun printBookInfo(title: String, author: String, yearPublished: Int) {
+    println("Title: $title") // cetak judul dari buku yang dipanggil dari parameter
+    println("Author: $author") // cetak penulis dari buku yang dipanggil dari parameter
+    println("Year Published: $yearPublished")  // cetak tahun dipublish dari buku yang dipanggil dari parameter
+    println()
+}
